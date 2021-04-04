070b0f724374b8354f6814d5eccf3a2c75d061e1
Eran Nussinovitch <eran.nussinovitch@gmail.com>
2021-04-04 17:00:18 -0400

    argparse

--- run_baseline.py
+++ run_baseline.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 from data.squad_utils import *
 from experiments.utils import *
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parsing Args..')
+    parser.add_argument('--bpath', type=str, nargs='1', required=False,
+                        help='base path', default='/cs/labs/gabis/ednussi/')
+    parser.add_argument('--max_samples_exp', type=int, nargs='1', required=False , default=9,
+                        help='The exponent used on a base 2 to determine max number of #samples takes')
+    parser.add_argument('--exp_repeat', type=int, nargs='1', required=False, default=5,
+                        help='Repeat experiment this number of times, use a different random seed for each exp')
+    parser.add_argument('--name', type=str, nargs='1', required=True, help='Experiment name')
+
+
+    args = parser.parse_args()
+    return args
 
 def create_folder(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 def run_base():
+    args = parse_args()
     squad_df = load_squad_df(squad_json_path)
 
-    base_results_path = 'results/'
+    base_path = args.bpath
     exp_name = 'baseline'
     base_model = "roberta-base"
     csv_entery_num = 0
@@ -18,16 +35,16 @@ def run_base():
     csv_columns = ['EM', 'F1', 'seed', 'n', 'base_model']
 
     # Create results folder and opens log
-    create_folder('results')
-    create_folder(f'results/{exp_name}')
-    create_folder(f'results/{exp_name}/data')
+    create_folder(f'{base_path}/results')
+    create_folder(f'{base_path}/results/{exp_name}')
+    create_folder(f'{base_path}/results/{exp_name}/data')
 
-    results_path = f'results/{exp_name}/results.csv'
+    results_path = f'{base_path}/results/{exp_name}/results.csv'
     with open(results_path, "a") as f:
         f.write(f',{",".join(csv_columns)}\n')
 
-    for n in get_log_scale(max_power_range=9):
-        for seed in range(5):
+    for n in get_log_scale(max_power_range=args.max_samples_exp):
+        for seed in range(args.exp_repeat):
 
             # ============ Train ============
             # pick q-a-c triplets at random
@@ -41,7 +58,7 @@ def run_base():
 
             # ============ Save ============
             # save answers df
-            answers_df_name = f'results/{exp_name}/data/answers-{n}_seed-{seed}.csv'
+            answers_df_name = f'{base_path}/results/{exp_name}/data/answers-{n}_seed-{seed}.csv'
             answers_df.to_csv(answers_df_name)
             EM, F1 = [x.split(':')[1] for x in res_summary.split(',')]
             with open(results_path, "a") as myfile:
@@ -50,8 +67,8 @@ def run_base():
             csv_entery_num += 1
 
     # ============ Plot Results ============
-    plot_random_sample_res([results_path], exp_name)
-    print('Expirement Run Succesfully')
+    plot_random_sample_res([results_path], exp_name, base_path)
+    print('Experiment Run Successfully')
 
 if __name__ == '__main__':
     run_base()
